4c755cf7808a92e420467aeaec798919784dbad4
Al Farizi <cyberunited92@gmail.com>
2018-12-31 14:33:51 +0700

    Slice Template

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,8 +5,8 @@ class Dashboard extends CI_Controller {
 
 	public function index()
 	{
-
+		$this->load->view('admin/layout/header');
 		$this->load->view('admin/dashboard');
-
+		$this->load->view('admin/layout/footer');
 	}
 }
